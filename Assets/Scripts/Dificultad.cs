6d8098d2f9d776ed01a84e8e6c4b209887e11a23
alepach05 <a.pacheco.v05@gmail.com>
2022-09-01 19:21:27 -0600

    Initial commit :tada:

---
+++ Assets/Scripts/Dificultad.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dificultad
+{
+    public string nombreDificultad;
+
+    public Pregunta[] preguntas;
+}
---
+++ Assets/Scripts/PanelComplementario.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelComplementario : MonoBehaviour
+{
+	public GameObject panelComplementario;
+	
+	public Game game;
+
+	public void continuar() 
+	{
+		panelComplementario.SetActive(false);
+		game.setPregunta();
+	}
+
+	public void desplegar() 
+	{
+		panelComplementario.SetActive(true);
+	}
+}
